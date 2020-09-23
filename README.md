dfeae1246f95daab27d8f33fda4aa8000cda97cd
Jacob Norton <jacob.norton@nortonprojects.com>
2020-09-23 14:14:43 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,12 @@
+Astro UXDS Tailwind Boilerplate
+
+Example tailwind config to stay consistent with astro color scheme
+
+# Installing and Building
+
+- `npm install`
+- `npm run build`
+
+# Serving
+
+- `python3 -m http.server` will serve the folder, then open localhost:8080
---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+
+const path =  require('path');
+const extract = require("mini-css-extract-plugin");
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.(scss|css)$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                                ident: 'postcss',
+                                syntax: 'postcss-scss',
+                                plugins: [
+                                    require('postcss-import'),
+                                    require('tailwindcss'),
+                                    require('autoprefixer'),
+                                ]
+                            }
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [
+                  {
+                    loader: 'file-loader',
+                    options: {
+                      name: '[name].[ext]',
+                      outputPath: 'fonts/'
+                    }
+                  }
+                ]
+              }
+        ]
+    },
+    plugins: [
+        new extract({
+            filename: 'bundle.css'
+        })
+    ],
+    mode: 'development'
+}
